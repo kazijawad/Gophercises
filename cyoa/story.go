db9cef0973405131f678181f71c561c32b450368
Kazi Jawad <kazi_jawad@outlook.com>
2019-09-25 21:30:12 -0400

    Add comments to exported code

--- cyoa/story.go
+++ cyoa/story.go
@@ -1,3 +1,5 @@
+// Package cyoa is for building Choose Your Own Adventure
+// stories that can be rendered via the resulting http.Handler
 package cyoa
 
 import (
@@ -76,20 +78,32 @@ var defaultHanderTmpl = `
 </html>
 `
 
+// HandlerOption is used with the NewHandler function to
+// configure the returned http.Handler.
 type HandlerOption func(h *handler)
 
+// WithTemplate is an option to provide a custom template to
+// be used when rendering stories.
 func WithTemplate(t *template.Template) HandlerOption {
 	return func(h *handler) {
 		h.t = t
 	}
 }
 
+// WithPathFunc is an option to provide a custom function
+// for processing the story chapter from the incoming request.
 func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
 	return func(h *handler) {
 		h.pathFn = fn
 	}
 }
 
+// NewHandler will construct an http.Handler that will render
+// the story provided.
+// The default handler will use the full path (minus the / prefix)
+// as the chapter name, defaulting to "intro" if the path is
+// empty. The default template creates option links that follow
+// this pattern.
 func NewHandler(s Story, opts ...HandlerOption) http.Handler {
 	h := handler{s, tpl, defaultPathFn}
 	for _, opt := range opts {
@@ -126,6 +140,9 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Chapter not found.", http.StatusNotFound)
 }
 
+// JSONStory will decode a story using the incoming reader
+// and the encoding/json package. It is assumed that the
+// provided reader has the story stored in JSON.
 func JSONStory(r io.Reader) (Story, error) {
 	d := json.NewDecoder(r)
 	var story Story
@@ -135,14 +152,27 @@ func JSONStory(r io.Reader) (Story, error) {
 	return story, nil
 }
 
+// Story represents a Choose Your Own Adventure story.
+// Each key is the name of a story chapter, and
+// each value is a Chapter.
 type Story map[string]Chapter
 
+// Chapter represents a CYOA story chapter. Each
+// chapter includes a title, the paragraphs it is composed
+// of, and options available for the reader to take at the
+// end of the chapter. If the options are empty it is
+// assumed that you have reached the end of that particular
+// story path.
 type Chapter struct {
 	Title      string   `json:"title"`
 	Paragraphs []string `json:"story"`
 	Options    []Option `json:"options"`
 }
 
+// Option represents a choice offered at the end of a story
+// chapter. Text is the visible text end users will see,
+// while the Chapter field will be the key to a chapter
+// stored in the Story object this chapter was found in.
 type Option struct {
 	Text    string `json:"text"`
 	Chapter string `json:"arc"`

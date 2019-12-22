cec444f4052f6f43e60c9d313d0f434294a071d7
Kazi Jawad <kazi.a.jawad@gmail.com>
2019-12-22 00:37:36 -0500

    Parse and build links for sitemap

--- sitemap/main.go
+++ sitemap/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
+	"net/url"
+	"strings"
+
+	"github.com/kazijawad/Gophercises/link"
 )
 
 func main() {
@@ -18,5 +20,25 @@ func main() {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	io.Copy(os.Stdout, resp.Body)
+
+	reqURL := resp.Request.URL
+	baseURL := &url.URL{
+		Scheme: reqURL.Scheme,
+		Host:   reqURL.Host,
+	}
+	base := baseURL.String()
+
+	links, _ := link.Parse(resp.Body)
+	var hrefs []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		}
+	}
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
 }

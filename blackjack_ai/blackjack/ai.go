a46cb0ae0d41d810f7e28c720ddafdcf38ad79fb
Kazi Jawad <kazi.a.jawad@gmail.com>
2020-01-08 17:08:56 -0500

    Start blackjack ai

---
+++ blackjack_ai/blackjack/ai.go
@@ -0,0 +1,55 @@
+package blackjack
+
+import (
+	"fmt"
+
+	"github.com/kazijawad/Gophercises/deck"
+)
+
+type AI interface {
+	Bet() int
+	Play(hand []deck.Card, dealer deck.Card) Move
+	Results(hand [][]deck.Card, dealer []deck.Card)
+}
+
+type HumanAI struct{}
+
+func (ai *HumanAI) Bet() int {
+	return 1
+}
+
+func (ai *HumanAI) Play(hand []deck.Card, dealer deck.Card) Move {
+	for {
+		fmt.Println("Player:", hand)
+		fmt.Println("Dealer:", dealer)
+		fmt.Println("What will you do? (h)it, (s)tand")
+		var input string
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			return Hit
+		case "s":
+			return Stand
+		default:
+			fmt.Println("Invalid Option:", input)
+		}
+	}
+}
+
+func (ai *HumanAI) Results(hand [][]deck.Card, dealer []deck.Card) {
+	fmt.Println("==FINAL HANDS==")
+	fmt.Println("Player:", hand)
+	fmt.Println("Dealer:", dealer)
+}
+
+type Move func(GameState) GameState
+
+type GameState struct{}
+
+func Hit(gs GameState) GameState {
+	return gs
+}
+
+func Stand(gs GameState) GameState {
+	return gs
+}

da16c9adbf2ab9a34c8d456eac22373b4da38480
Kazi Jawad <kazi.a.jawad@gmail.com>
2020-02-09 00:26:35 -0500

    Add splitting to blakjack ai

--- blackjack_ai/blackjack/ai.go
+++ blackjack_ai/blackjack/ai.go
@@ -9,7 +9,7 @@ import (
 type AI interface {
 	Bet(shuffled bool) int
 	Play(hand []deck.Card, dealer deck.Card) Move
-	Results(hand [][]deck.Card, dealer []deck.Card)
+	Results(hands [][]deck.Card, dealer []deck.Card)
 }
 
 type dealerAI struct{}
@@ -27,7 +27,7 @@ func (ai dealerAI) Play(hand []deck.Card, dealer deck.Card) Move {
 	return MoveStand
 }
 
-func (ai dealerAI) Results(hand [][]deck.Card, dealer []deck.Card) {
+func (ai dealerAI) Results(hands [][]deck.Card, dealer []deck.Card) {
 	// NOOP
 }
 
@@ -51,7 +51,7 @@ func (ai humanAI) Play(hand []deck.Card, dealer deck.Card) Move {
 	for {
 		fmt.Println("Player:", hand)
 		fmt.Println("Dealer:", dealer)
-		fmt.Println("What will you do? (h)it, (s)tand, (d)ouble")
+		fmt.Println("What will you do? (h)it, (s)tand, (d)ouble, s(p)lit")
 		var input string
 		fmt.Scanf("%s\n", &input)
 		switch input {
@@ -61,14 +61,19 @@ func (ai humanAI) Play(hand []deck.Card, dealer deck.Card) Move {
 			return MoveStand
 		case "d":
 			return MoveDouble
+		case "p":
+			return MoveSplit
 		default:
 			fmt.Println("Invalid Option:", input)
 		}
 	}
 }
 
-func (ai humanAI) Results(hand [][]deck.Card, dealer []deck.Card) {
+func (ai humanAI) Results(hands [][]deck.Card, dealer []deck.Card) {
 	fmt.Println("==FINAL HANDS==")
-	fmt.Println("Player:", hand)
+	fmt.Println("Player:")
+	for _, h := range hands {
+		fmt.Println(" ", h)
+	}
 	fmt.Println("Dealer:", dealer)
 }

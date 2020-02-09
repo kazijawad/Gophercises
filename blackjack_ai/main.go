3ded09c4cc63ea002a0f3b9ad7580f2e357fcb4d
Kazi Jawad <kazi.a.jawad@gmail.com>
2020-02-09 00:34:19 -0500

    Fix errors in blackjack ai

--- blackjack_ai/main.go
+++ blackjack_ai/main.go
@@ -4,7 +4,22 @@ import (
 	"fmt"
 
 	"github.com/kazijawad/Gophercises/blackjack_ai/blackjack"
+	"github.com/kazijawad/Gophercises/deck"
 )
+
+type basicAI struct{}
+
+func (ai *basicAI) Bet(shuffled bool) int {
+	panic("not implemented") // TODO: Implement
+}
+
+func (ai *basicAI) Play(hand []deck.Card, dealer deck.Card) blackjack.Move {
+	panic("not implemented") // TODO: Implement
+}
+
+func (ai *basicAI) Results(hands [][]deck.Card, dealer []deck.Card) {
+	panic("not implemented") // TODO: Implement
+}
 
 func main() {
 	opts := blackjack.Options{
